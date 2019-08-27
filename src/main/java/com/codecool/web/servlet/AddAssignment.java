64312948738fa5e9e4e9f5e74f4272cb9a2a48bb
turo62 <turo62@gmail.com>
2019-08-27 14:13:22 +0200

    Displaying texts and assignments are mainly ready.

--- src/main/java/com/codecool/web/servlet/AddAssignment.java
+++ src/main/java/com/codecool/web/servlet/AddAssignment.java
@@ -6,35 +6,38 @@ import com.codecool.web.model.user.User;
 import com.codecool.web.service.AssignmentService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.SQLClientInfoException;
 import java.sql.SQLException;
 
+
+@WebServlet("/protected/assignment")
 public class AddAssignment extends AbstractServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         resp.setContentType("text/plain");
-        
-        
+    
+    
         try (Connection connection = getConnection(req.getServletContext())){
             DBAssignmentDao assignmentDao = new DBAssignmentDao(connection);
             AssignmentService assignmentService = new AssignmentService(assignmentDao);
-    
-    
+        
+        
             HttpSession session = req.getSession(false);
             User user = (User) session.getAttribute("user");
-    
+        
             if (req.getParameter("question") != null) {
                 String title = req.getParameter("title");
                 String question = req.getParameter("question");
                 int maxScore = Integer.getInteger("max_Score");
                 Boolean isPublished = Boolean.getBoolean(req.getParameter("is_published"));
-    
+            
                 Assignment assignment = new Assignment(title, question, isPublished, maxScore);
             }
         } catch (SQLException e) {
