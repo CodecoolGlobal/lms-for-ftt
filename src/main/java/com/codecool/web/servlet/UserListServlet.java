482caedc2549ea86983f1694540eeb2997a5946a
belu82 <adam.beluscsak@gmail.com>
2019-08-29 14:10:51 +0200

    User list is ready

---
+++ src/main/java/com/codecool/web/servlet/UserListServlet.java
@@ -0,0 +1,30 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.DAO.DBUserDao;
+import com.codecool.web.model.user.User;
+import com.codecool.web.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/userlist")
+public class UserListServlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try (Connection connection = getConnection(request.getServletContext())) {
+            request.setAttribute("userList", DBUserDao.getUsersList(connection));
+
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        request.getRequestDispatcher("userList.jsp").forward(request, response);
+    }
+}
